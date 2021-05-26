5d20f5e729f73a8139070db794a4c56990e10559
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-05-26 23:52:19 +0900

    keys.goにTwitter Apiのキー情報を記載
    Twitter Apiの情報を取得する為

--- keys/keys.go
+++ keys/keys.go
@@ -5,8 +5,8 @@ import (
 )
 
 func GetTwitterApi() *anaconda.TwitterApi {
-	anaconda.SetConsumerKey("")
-	anaconda.SetConsumerSecret("")
-	api := anaconda.NewTwitterApi("", "")
+	anaconda.SetConsumerKey("FHQ9hHYK3nP959weYdp8WvBFF")
+	anaconda.SetConsumerSecret("lwO5AGwyYqNmGVN2b0BQyIy2wfC820jHk3JWi0lJkpp6yteny9")
+	api := anaconda.NewTwitterApi("AAAAAAAAAAAAAAAAAAAAABEvQAEAAAAA5Dar6zhKgUAwe4UNsqknjTIbZFs%3DMGBbwv0a8Eh27YywgmyWhiQqSsY6Cl173qNfy3D0HNKEv0HKjC")
 	return api
 }
