c757740eaf1a934eb74d7744ce9bc58b072a6751
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-05-26 23:57:15 +0900

    main.goを記載
    Twitter botで投稿する為

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+"fmt"
+"time"
+. "fmt"
+. "./keys"
+. "github.com/***/tweetbot/text"
+)
+
+func main() {
+    chStop := make(chan int, 1)
+    TimerFunc(chStop)
+
+    time.Sleep(time.Hour * 24 * 365)
+    chStop <- 0
+
+    close(chStop)
+
+    time.Sleep(time.Second * 1)
+
+    fmt.Println("Application End.")
+}
+
+func TimerFunc(stopTimer chan int) {
+    go func() {
+        ticker := time.NewTicker(24 * time.Hour)
+
+    LOOP:
+        for {
+            select {
+            case <-ticker.C:
+                api := GetTwitterApi()
+                text := ChooseTweet()
+
+                tweet, err := api.PostTweet(text, nil)
+                if err != nil {
+                    panic(err)
+                }
+                Print(tweet.Text)
+                //}
+            case <-stopTimer:
+                fmt.Println("Timer stop.")
+                ticker.Stop()
+                break LOOP
+            }
+        }
+        fmt.Println("timerfunc end.")
+    }()
+}
